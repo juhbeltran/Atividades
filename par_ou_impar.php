9dd0840b2e9b32cad899fb60b54fa1410e969910
juhbeltran <juliabeltran14@gmail.com>
2021-10-21 22:36:15 -0300

    alterei arquivo par ou impar

--- par_ou_impar.php
+++ par_ou_impar.php
@@ -9,6 +9,7 @@ $e = isset($_POST['e']) ? $_POST['e'] : "";
 ?>
 <html lang="PT-BR">
 <head>
+    
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=, initial-scale=1.0">
